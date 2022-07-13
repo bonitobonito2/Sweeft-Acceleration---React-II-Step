420984c3d7675c99cb2c34ee112e2060b7be731c
Zaal tsagareli <zaal.tsagareli.1@btu.edu.ge>
2022-07-13 15:11:02 +0400

    .

--- src/components/SingleUser/friends/Friends.js
+++ src/components/SingleUser/friends/Friends.js
@@ -1,5 +1,5 @@
-import React, { useEffect} from "react";
-import { useParams} from "react-router-dom";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import classes from "./Friends.module.css";
 import ScrollLoading from "../../shared/loadingWhileScroll/ScrollLoading";
 import Card from "../../shared/card/Card";
@@ -7,28 +7,39 @@ import UseHttpHook from "../../../hooks/UseHttpHook";
 import UseUserClickHandlerHook from "../../../hooks/UseUserClickHandlerHook";
 function Friends() {
   const { id } = useParams();
-  const clickListener = UseUserClickHandlerHook()
-  const {error,data,page,scrollLoading,
-       getData,setData,} = UseHttpHook();
+  const clickListener = UseUserClickHandlerHook();
+  const { error, data, page, scrollLoading, getData, setData } = UseHttpHook();
 
   useEffect(() => {
     setData([]);
-    getData(`http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${id}/friends/1/16`);
+    getData(
+      `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${id}/friends/1/16`
+    );
   }, [id, getData, setData]);
 
-  window.onscroll = () => {
+  window.onscroll = async () => {
     if (
       window.innerHeight + document.documentElement.scrollTop >
       document.documentElement.offsetHeight - 1
-    ) getData(`http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${id}/friends/${page}/8`)
+    )
+    await  getData(
+        `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${id}/friends/${page}/8`
+      );
   };
 
   if (error || !data) return <div>something went wrong</div>;
   return (
     <div className={classes.friends}>
-      {data.map((state) => (
-        <Card onClick={clickListener} key={state.id} info={state} />
-      ))}
+      {data.map((state) => {
+        const randomKeyHalper = Math.random();
+        return (
+          <Card
+            onClick={clickListener}
+            key={state.id + randomKeyHalper}
+            info={state}
+          />
+        );
+      })}
       {scrollLoading && <ScrollLoading />}
     </div>
   );
--- src/components/all-users/Users.js
+++ src/components/all-users/Users.js
@@ -1,31 +1,43 @@
-import React, { Fragment, useEffect} from "react";
+import React, { Fragment, useEffect } from "react";
 import Card from "../shared/card/Card";
 import ScrollLoading from "../shared/loadingWhileScroll/ScrollLoading";
 import UseHttpHook from "../../hooks/UseHttpHook";
-import classes from './Users.module.css'
+import classes from "./Users.module.css";
 import UseUserClickHandlerHook from "../../hooks/UseUserClickHandlerHook";
 function Users() {
-  const clickListener = UseUserClickHandlerHook()
-  const { error, data, page, scrollLoading, getData} = UseHttpHook();
-  
+  const clickListener = UseUserClickHandlerHook();
+  const { error, data, page, scrollLoading, getData } = UseHttpHook();
+
   useEffect(() => {
-    getData(`http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/16`);
+    getData(
+      `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/16`
+    );
   }, [getData]);
 
-  window.onscroll = () => {
+  window.onscroll = async () => {
     if (
       window.innerHeight + document.documentElement.scrollTop >
       document.documentElement.offsetHeight - 1
-    ) getData(`http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${page}/8`);
+    )
+     await getData(
+        `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${page}/8`
+      );
   };
-  if (error ) return <div>something went wrong</div>;
-  if(data.length === 0) return <ScrollLoading />
+  if (error) return <div>something went wrong</div>;
+  if (data.length === 0) return <ScrollLoading />;
   return (
     <Fragment>
       <div className={classes.People}>
-        {data.map((state) => (
-          <Card onClick={clickListener} key={state.id} info={state} />
-        ))}
+        {data.map((state) => {
+          const randomKeyHalper = Math.random();
+          return (
+            <Card
+              onClick={clickListener}
+              key={state.id + randomKeyHalper}
+              info={state}
+            />
+          );
+        })}
       </div>
       {scrollLoading && <ScrollLoading />}
     </Fragment>
